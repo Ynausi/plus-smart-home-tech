83b6ec3435ea0e411bf6305cd0ccc6620da348b4
Ynausi <trot55636@gmail.com>
2026-07-19 23:26:45 +0300

    fix remarks

---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/mapper/HubEventConverter.java
@@ -0,0 +1,79 @@
+package ru.yandex.practicum.mapper;
+
+import com.google.protobuf.Timestamp;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.dto.hub.*;
+import ru.yandex.practicum.grpc.telemetry.event.*;
+import ru.yandex.practicum.kafka.telemetry.hub.*;
+
+import java.time.Instant;
+
+@Component
+@RequiredArgsConstructor
+public class HubEventConverter {
+
+    private static final int BOOLEAN_TRUE = 1;
+    private static final int BOOLEAN_FALSE = 0;
+
+    private final HubEventMapper mapper;
+
+    public HubEventDto fromProto(HubEventProto proto) {
+        return switch (proto.getPayloadCase()) {
+            case DEVICE_ADDED -> {
+                DeviceAddedEventDto dto = mapper.fromProto(proto.getDeviceAdded());
+                fillCommonFields(dto, proto);
+                yield dto;
+            }
+            case DEVICE_REMOVED -> {
+                DeviceRemovedEventDto dto = mapper.fromProto(proto.getDeviceRemoved());
+                fillCommonFields(dto, proto);
+                yield dto;
+            }
+            case SCENARIO_ADDED -> {
+                ScenarioAddedEventDto dto = mapper.fromProto(proto.getScenarioAdded());
+                fillCommonFields(dto, proto);
+                yield dto;
+            }
+            case SCENARIO_REMOVED -> {
+                ScenarioRemovedEventDto dto = mapper.fromProto(proto.getScenarioRemoved());
+                fillCommonFields(dto, proto);
+                yield dto;
+            }
+            case PAYLOAD_NOT_SET ->
+                    throw new IllegalArgumentException("Payload not set");
+        };
+    }
+
+
+    public HubEventAvro toAvro(HubEventDto dto) {
+        Object payload = switch (dto.getType()) {
+            case DEVICE_ADDED ->
+                    mapper.toAvro((DeviceAddedEventDto) dto);
+            case DEVICE_REMOVED ->
+                    mapper.toAvro((DeviceRemovedEventDto) dto);
+            case SCENARIO_ADDED ->
+                    mapper.toAvro((ScenarioAddedEventDto) dto);
+            case SCENARIO_REMOVED ->
+                    mapper.toAvro((ScenarioRemovedEventDto) dto);
+        };
+
+        return HubEventAvro.newBuilder()
+                .setHubId(dto.getHubId())
+                .setTimestamp(dto.getTimestamp())
+                .setPayload(payload)
+                .build();
+    }
+
+    private void fillCommonFields(HubEventDto dto, HubEventProto proto) {
+        dto.setHubId(proto.getHubId());
+        dto.setTimestamp(fromTimestamp(proto.getTimestamp()));
+    }
+
+    private Instant fromTimestamp(Timestamp timestamp) {
+        return Instant.ofEpochSecond(
+                timestamp.getSeconds(),
+                timestamp.getNanos()
+        );
+    }
+}
--- telemetry/collector/src/main/java/ru/yandex/practicum/service/sensor/SensorServiceImpl.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/service/sensor/SensorServiceImpl.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.service.sensor;
 import ru.yandex.practicum.dto.sensor.SensorEventDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import ru.yandex.practicum.mapper.SensorEventConverter;
 import ru.yandex.practicum.mapper.SensorEventMapper;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -16,12 +17,12 @@ public class SensorServiceImpl implements SensorService{
 
     private final KafkaTemplate<String, SensorEventAvro> kafkaTemplate;
     private static final String SENSOR_TOPIC = "telemetry.sensors.v1";
-    private final SensorEventMapper sensorMapper;
+    private final SensorEventConverter sensorEventConverter;
 
     @Override
     public void createSensor(SensorEventProto proto) {
-        SensorEventDto dto = sensorMapper.fromProto(proto);
-        SensorEventAvro avroEvent = sensorMapper.toAvro(dto);
+        SensorEventDto dto = sensorEventConverter.fromProto(proto);
+        SensorEventAvro avroEvent = sensorEventConverter.toAvro(dto);
         log.info("id = {},type = {}",dto.getId(),dto.getType());
         kafkaTemplate.send(SENSOR_TOPIC,dto.getHubId(),avroEvent);
     }
